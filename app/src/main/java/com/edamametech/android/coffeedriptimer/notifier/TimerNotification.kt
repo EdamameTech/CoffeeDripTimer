ee302dd65480872dad2842fb2f1b5762c37f7316
zunda <zundan@gmail.com>
2024-07-06 10:52:36 -1000

    Show app when notification is tapped

--- app/src/main/java/com/edamametech/android/coffeedriptimer/notifier/TimerNotification.kt
+++ app/src/main/java/com/edamametech/android/coffeedriptimer/notifier/TimerNotification.kt
@@ -2,7 +2,9 @@ package com.edamametech.android.coffeedriptimer.notifier
 
 import android.annotation.SuppressLint
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.edamametech.android.coffeedriptimer.MainActivity
@@ -14,12 +16,18 @@ class TimerNotification(val context: Context?) {
     @SuppressLint("MissingPermission")
     fun showTimerNotification(message: String) {
         if (context != null) {
-
+            val showAppIntent = Intent(context, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+            }
+            val showAppPendingIntent: PendingIntent = PendingIntent.getActivity(
+                context, intentId, showAppIntent, PendingIntent.FLAG_IMMUTABLE
+            )
             val builder = NotificationCompat.Builder(context, MainActivity.TIMER_CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle(context.getString(R.string.app_name))
                 .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(showAppPendingIntent)
                 .setAutoCancel(true)
 
             NotificationManagerCompat.from(context).notify(notificationId, builder.build())
