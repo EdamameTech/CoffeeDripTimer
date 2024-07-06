235c3d0827a1dee2896ce6b0939889c2bbd0cf6b
zunda <zundan@gmail.com>
2024-07-05 20:47:21 -1000

    Implement user interface

--- app/src/main/java/com/edamametech/android/coffeedriptimer/MainActivity.kt
+++ app/src/main/java/com/edamametech/android/coffeedriptimer/MainActivity.kt
@@ -1,47 +1,36 @@
 package com.edamametech.android.coffeedriptimer
 
+import CoffeeDripTimerScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.edamametech.android.coffeedriptimer.ui.theme.CoffeeDripTimerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             CoffeeDripTimerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    CoffeeDripTimerScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoffeeDripTimerTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/edamametech/android/coffeedriptimer/ui/CoffeeDripTimerScreen.kt
@@ -0,0 +1,239 @@
+import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.edamametech.android.coffeedriptimer.R
+import com.edamametech.android.coffeedriptimer.ui.theme.CoffeeDripTimerTheme
+
+enum class RoastOfBeans(val label: Int) {
+    MEDIUM(R.string.medium_roast),
+    DARK(R.string.dark_roast)
+}
+
+data class BrewStep (
+    val waterAmountFactor: Double,
+    val waitDurationFactor: Long
+)
+
+const val waitDurationUnit = 5000L
+// in msec, TODO: 60000L (1 minute) for release
+
+enum class BrewStepTypes(val step: BrewStep) {
+    SHORT(BrewStep(5.0, 2)),
+    LONG(BrewStep(7.5, 3))
+}
+
+val brewSteps = mapOf(
+    RoastOfBeans.MEDIUM to arrayOf(
+        BrewStepTypes.SHORT,
+        BrewStepTypes.LONG,
+        BrewStepTypes.LONG
+        ),
+    RoastOfBeans.DARK to arrayOf(
+        BrewStepTypes.SHORT,
+        BrewStepTypes.SHORT,
+        BrewStepTypes.SHORT,
+        BrewStepTypes.SHORT
+    )
+)
+
+@Composable
+fun CoffeeDripTimerScreen(modifier: Modifier = Modifier) {
+    var amountOfBeans by rememberSaveable { mutableStateOf("10") }
+    var roastOfBeans by rememberSaveable { mutableStateOf(RoastOfBeans.DARK) }
+    var startedAt:Long? by rememberSaveable { mutableStateOf(null) }
+
+    fun updateAmount(amount: String) {
+        if (Regex("\\d*([.]\\d*)?").matches(amount)) {
+            amountOfBeans = amount
+        }
+    }
+
+    fun updateRoast(roast: RoastOfBeans) {
+        roastOfBeans = roast
+    }
+
+    fun startTimer() {
+        startedAt = System.currentTimeMillis()
+    }
+
+    fun cancelTimer() {
+        startedAt = null
+    }
+
+    Column {
+        Row {
+            AmountOfBeansInput(
+                amount = amountOfBeans,
+                enabled = (startedAt == null),
+                onValueChange = { updateAmount(it) },
+                modifier = Modifier.weight(0.5F)
+            )
+            RoastOfBeansInput(
+                roast = roastOfBeans,
+                enabled = (startedAt == null),
+                onValueChange = { updateRoast(it) },
+                modifier = Modifier.weight(0.5F)
+            )
+        }
+        BrewStepsDisplay(
+            amount = amountOfBeans,
+            roast = roastOfBeans
+        )
+        Row {
+            StartTimerButton(
+                onClick = { startTimer() },
+                enabled = (startedAt == null),
+                modifier = Modifier.weight(0.75F)
+            )
+            CancelTimerButton(
+                onClick = { cancelTimer() },
+                enabled = (startedAt != null),
+                modifier = Modifier.weight(0.25F)
+            )
+        }
+    }
+}
+
+@Composable
+fun AmountOfBeansInput(
+    amount: String,
+    enabled: Boolean,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = amount,
+        enabled = enabled,
+        label = { Text(stringResource(R.string.amount_of_beans)) },
+        onValueChange = onValueChange,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun RoastOfBeansInput(
+    roast: RoastOfBeans,
+    enabled: Boolean,
+    onValueChange: (RoastOfBeans) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Button(
+            enabled = enabled,
+            onClick = { expanded = true },
+            content = {
+                Text(stringResource(roast.label))
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            for (x in RoastOfBeans.entries) {
+                if (x != roast) {
+                    DropdownMenuItem(
+                        text = { Text(stringResource(x.label)) },
+                        onClick = {
+                            onValueChange(x)
+                            expanded = false
+                        }
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun BrewStepsDisplay(
+    amount: String,
+    roast: RoastOfBeans,
+    modifier: Modifier = Modifier
+) {
+    var targetAmount = 0.0
+    val beans = amount.toDouble()
+    val nSteps = brewSteps[roast]?.size ?: 0
+    Column {
+        for ((i, s) in (brewSteps[roast] ?: arrayOf<BrewStepTypes>()).withIndex()) {
+            Log.d("BrewStepsDisplay", String.format("i:%d s:%s", i, s.toString()))
+            targetAmount += s.step.waterAmountFactor * beans
+            Text(
+                text = String.format(stringResource(R.string.step_amount_format), targetAmount)
+            )
+            if (i < nSteps - 1) {
+                val remaining = s.step.waitDurationFactor * waitDurationUnit
+                val min = (remaining + 500) / 1000 / 60
+                val sec = ((remaining + 500) / 1000).rem(60)
+                Text(
+                    text = String.format(stringResource(R.string.step_wait_format), min, sec),
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Right
+
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun StartTimerButton(
+    enabled: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        enabled = enabled,
+        onClick = onClick,
+        content = { Text(stringResource(R.string.start)) },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun CancelTimerButton(
+    enabled: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        enabled = enabled,
+        onClick = onClick,
+        content = { Text(stringResource(R.string.cancel)) },
+        modifier = modifier
+    )
+}
+@Preview(showBackground = true)
+@Composable
+fun CoffeeDripTimerScreenPreview() {
+    CoffeeDripTimerTheme {
+        CoffeeDripTimerScreen()
+    }
+}
