6214f4e78a48ac811fb12f05e2597de727842083
zunda <zundan@gmail.com>
2024-07-07 15:26:37 -1000

    Remove unreferenced resources

--- app/src/main/java/com/edamametech/android/coffeedriptimer/notifier/ScheduleTImerNotification.kt
+++ app/src/main/java/com/edamametech/android/coffeedriptimer/notifier/ScheduleTImerNotification.kt
@@ -5,7 +5,6 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.util.Log
 import com.edamametech.android.coffeedriptimer.notifier.TimerReceiver.Companion.MESSAGE
 
 fun pendingIntentForTimerNotification(context: Context, requestCode: Int, message: String = ""): PendingIntent {
