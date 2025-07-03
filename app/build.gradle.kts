0f633e75da7910ee442d0ddb0fab682c9228e53f
zunda <zundan@gmail.com>
2025-07-03 11:23:30 -1000

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.edamametech.android.coffeedriptimer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.3.1"
+        versionCode = 11
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
